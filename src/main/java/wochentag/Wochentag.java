30a5b244de610459cfbc948d005cbf2042836965
lnguyen2 <van.loc-nguyen@immonet.de>
2019-04-24 17:40:53 +0200

    create class and code to convert number to weekday.

---
+++ src/main/java/wochentag/Wochentag.java
@@ -0,0 +1,37 @@
+package wochentag;
+
+import java.util.Scanner;
+
+public class Wochentag {
+
+    public static void main(String[] args) {
+        Wochentag wochentag = new Wochentag();
+        System.out.println("Type in a number from 1 to 7. The number will appear as a weekday.");
+        System.out.println(wochentag.übersetze());
+    }
+
+    public String übersetze() {
+        int number = new Scanner(System.in).nextInt();
+
+        switch (number) {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Satuday";
+            case 7:
+                return "Sunday";
+            default:
+                return "You didn't type a valid number";
+        }
+
+    }
+}
