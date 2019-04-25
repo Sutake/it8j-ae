ecee076cacdc1ac2cdb2a5e64401a54e0f596610
lnguyen2 <van.loc-nguyen@immonet.de>
2019-04-25 13:09:50 +0200

    delete unnecessary comments

--- src/main/java/modul4/PortoRechner.java
+++ src/main/java/modul4/PortoRechner.java
@@ -7,28 +7,16 @@ public class PortoRechner {
     public static void main(String[] args) {
         PortoRechner portoRechner = new PortoRechner();
 
-        System.out.println("Geben Sie das Gewicht ein: ");
-        int gewicht = new Scanner(System.in).nextInt();
-        if (portoRechner.berechnePorto(gewicht) >= 1000) {
-            System.out.println("Sendung wird als Paket oder Päckchen versendet.");
-        } else {
-            System.out.println("Sie müssen " + portoRechner.berechnePorto(gewicht) + "€ Porto/Versand bezahlen");
-        }
+        System.out.println("Geben Sie die Gesamtsumme ein. Wir werden Ihnen die Porto- und Versandkosten berechnen: ");
+        int auftragssumme = new Scanner(System.in).nextInt();
+        System.out.println("Sie müssen " + portoRechner.berechneKosten(auftragssumme) + "€ Porto/Versand bezahlen");
     }
 
-    public double berechnePorto(int gewicht) {
-        if (gewicht > 0 && gewicht < 20) {
-            return 1;
-        } else if (gewicht >= 20 && gewicht < 50) {
-            return 1.7;
-        } else if (gewicht >= 50 && gewicht < 100) {
-            return 2.4;
-        } else if (gewicht >= 100 && gewicht < 250) {
-            return 3.2;
-        } else if (gewicht >= 250 && gewicht < 500) {
-            return 4;
-        } else if (gewicht >= 500 && gewicht < 1000) {
-            return 4.8;
+    public double berechneKosten(double auftragsumme) {
+        if (auftragsumme < 100) {
+            return 5.5;
+        } else if (auftragsumme >= 100 && auftragsumme < 200) {
+            return 3;
         } else {
             return 0;
         }
