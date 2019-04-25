b4d07bc92ee28eae4447ac1fa84b5099842513b4
lnguyen2 <van.loc-nguyen@immonet.de>
2019-04-25 13:25:31 +0200

    create new class

---
+++ src/main/java/modul4/Luftmessung.java
@@ -0,0 +1,38 @@
+package modul4;
+
+public class Luftmessung {
+    private double grenzwert;
+
+    public static void main(String[] args) {
+        Luftmessung luftmessung1 = new Luftmessung();
+        luftmessung1.setGrenzwert(1000);
+        luftmessung1.pruefe(900);
+
+        Luftmessung luftmessung2 = new Luftmessung();
+        luftmessung2.setGrenzwert(1000);
+        luftmessung2.pruefe(1700);
+    }
+
+    public Luftmessung() {
+    }
+
+    public double getGrenzwert() {
+        return grenzwert;
+    }
+
+    public void setGrenzwert(double grenzwert) {
+        this.grenzwert = grenzwert;
+    }
+
+    public void pruefe(double wert){
+        System.out.println("Der Genzwert beträgt " + grenzwert + " ppm");
+        System.out.println("Der Messwert beträgt " + wert + " ppm");
+        if (wert < this.grenzwert) {
+            System.out.println("Der Messwert liegt unterhalb des Grenzwertes");
+            System.out.println();
+        } else {
+            System.out.println("Der Messwert überschreitet den Grenzwert um " + (wert - grenzwert));
+            System.out.println();
+        }
+    }
+}
