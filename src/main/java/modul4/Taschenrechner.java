bd2e315f072f58acd28f527d0f94d33f0774acca
lnguyen2 <van.loc-nguyen@immonet.de>
2019-04-25 11:23:48 +0200

    create new classes.

---
+++ src/main/java/modul4/Taschenrechner.java
@@ -0,0 +1,64 @@
+package modul4;
+
+public class Taschenrechner {
+    private double zahl1;
+    private double zahl2;
+    private char operator;
+
+    public static void main(String[] args) {
+        Taschenrechner taschenrechner = new Taschenrechner();
+        taschenrechner.setZahl1(20);
+        taschenrechner.setZahl2(0);
+        taschenrechner.setOperator('/');
+        if (taschenrechner.getZahl2() == 0 && taschenrechner.getOperator() == '/') {
+            System.out.println("You cannot divide by zero");
+        } else {
+            taschenrechner.berechne();
+        }
+
+    }
+
+    public Taschenrechner() {
+
+    }
+
+    public double getZahl1() {
+        return zahl1;
+    }
+
+    public void setZahl1(double zahl1) {
+        this.zahl1 = zahl1;
+    }
+
+    public double getZahl2() {
+        return zahl2;
+    }
+
+    public void setZahl2(double zahl2) {
+        this.zahl2 = zahl2;
+    }
+
+    public char getOperator() {
+        return operator;
+    }
+
+    public void setOperator(char operator) {
+        this.operator = operator;
+    }
+
+    public double berechne() {
+        switch (operator) {
+            case '+':
+                return zahl1 + zahl2;
+            case '-':
+                return zahl1 - zahl2;
+            case '*':
+                return zahl1 * zahl2;
+            case '/':
+                return zahl1 / zahl2;
+
+            default:
+                return 0;
+        }
+    }
+}
