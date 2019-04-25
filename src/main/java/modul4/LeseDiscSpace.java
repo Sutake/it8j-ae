75340ab2f94fbee65f5857f187b1d60db6c6e5c3
lnguyen2 <van.loc-nguyen@immonet.de>
2019-04-25 15:01:13 +0200

    create class and convert memory number to GByte

---
+++ src/main/java/modul4/LeseDiscSpace.java
@@ -0,0 +1,14 @@
+package modul4;
+
+        import java.io.File;
+
+public class LeseDiscSpace {
+    public static void main(String[] args) {
+
+        //for Windows: pathname = "C:\\".
+        File f = new File("/Volumes");
+        System.out.println(f.getTotalSpace());
+        System.out.println((f.getTotalSpace() / 1024 / 1024 / 1024) + "GByte Gesamtspeicher");
+        System.out.println((f.getFreeSpace() / 1024 / 1024 / 1024) + "GByte freier Speicher");
+    }
+}
