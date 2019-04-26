89f7a1e10ff83c41d4092fed993df978c36d5899
lnguyen2 <van.loc-nguyen@immonet.de>
2019-04-26 13:02:02 +0200

    create further classes

---
+++ src/main/java/modul4/MatheAss.java
@@ -0,0 +1,16 @@
+package modul4;
+
+import java.util.Scanner;
+
+public class MatheAss {
+    public static void main(String[] args) {
+        System.out.println("Type in a number");
+        int result = 0;
+
+        String temp = Integer.toString(new Scanner(System.in).nextInt());
+        for (int i = 0; i < temp.length(); i++) {
+            result += temp.charAt(i) - '0';
+        }
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/modul4/Sofortrente.java
@@ -0,0 +1,32 @@
+package modul4;
+
+public class Sofortrente {
+
+    public static void main(String[] args) {
+        Sofortrente sofortrente = new Sofortrente(1000000,4);
+        sofortrente.berechneZeitraum();
+    }
+
+    private double zinssatz;
+    private double einlage;
+
+    public Sofortrente(double einlage, double zinssatz) {
+        this.zinssatz = zinssatz;
+        this.einlage = einlage;
+    }
+
+    public void berechneZeitraum() {
+       int jahre = 0;
+
+       while ( einlage > 0) {
+           einlage = einlage * (1 + zinssatz/100) - 60000;
+           jahre++;
+       }
+
+       if (einlage >0) {
+           System.out.println("Nach " + jahre + " ist die Einlage auf " + einlage + " geschmolzen.");
+       } else {
+           System.out.println("Nach "+ (jahre -1) + " Jahren musst Du wieder arbeiten!");
+       }
+    }
+}
