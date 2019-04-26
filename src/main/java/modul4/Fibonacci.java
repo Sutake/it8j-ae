739e278f24cfb3c049be2e0d45f02fe043c1c00c
lnguyen2 <van.loc-nguyen@immonet.de>
2019-04-26 13:59:01 +0200

    new class

---
+++ src/main/java/modul4/Fibonacci.java
@@ -0,0 +1,22 @@
+package modul4;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        fibonacci.berechneNächsteZahl();
+    }
+
+    private int x = 0;
+    private int y = 1;
+    private int z;
+
+
+    public void berechneNächsteZahl() {
+        while (z + x + y < 1000) {
+            z = x + y; // erg = die ersten beiden Zahlen
+            x = y; // gleich setzten von a und b
+            y = z; // b auf erg setzen damit die Summe der beiden Vorgänger gebildet werden
+            System.out.print(z + "\t");
+        }
+    }
+}
