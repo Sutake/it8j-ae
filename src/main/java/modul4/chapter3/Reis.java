0582e76ee00ef8b03fd5c3ed0f44f4274d47d922
lnguyen2 <van.loc-nguyen@immonet.de>
2019-05-02 15:47:49 +0200

    create new class with method

---
+++ src/main/java/modul4/chapter3/Reis.java
@@ -0,0 +1,19 @@
+package modul4.chapter3;
+
+public class Reis {
+    public static void main(String[] args) {
+        Reis reis = new Reis();
+        reis.summiere();
+
+    }
+
+    private double summe;
+
+    public double summiere() {
+        for (int i = 0; i <= 64; i++) {
+            summe += Math.pow(2, i);
+            System.out.println(summe);
+        }
+        return summe;
+    }
+}
