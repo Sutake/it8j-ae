9aaaa7671b1e60f23d26f6e636df91e2e148e3e0
lnguyen2 <van.loc-nguyen@immonet.de>
2019-06-13 14:26:45 +0200

    change method that results are printed out directly

--- src/main/java/modul4/chapter3/MatheProfi.java
+++ src/main/java/modul4/chapter3/MatheProfi.java
@@ -1,23 +1,15 @@
 package modul4.chapter3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class MatheProfi {
     public static void main(String[] args) {
         int x = 8;
         int i = x;
-        List<Integer> listInt = new ArrayList<>();
 
         while (i >= 1) {
             if (x % i == 0) {
-                listInt.add(i);
+                System.out.println(i);
             }
             i--;
         }
-        //used static method of class Collections to sort numbers.
-        Collections.sort(listInt);
-        listInt.forEach(System.out::println);
     }
 }
