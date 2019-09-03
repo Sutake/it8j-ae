07f8fdced8416f82d8db0687ba5e4c134f63d4b7
vlnguyen <van-loc.nguyen@immonet.de>
2019-09-03 09:43:02 +0200

    change algorithm for inserting

--- src/main/java/modul4/chapter4/Sortierer.java
+++ src/main/java/modul4/chapter4/Sortierer.java
@@ -1,22 +1,18 @@
 package modul4.chapter4;
 
+import java.util.Arrays;
+
 public class Sortierer {
 
-    public int[] insertNumberInArray(int a[], int number) {
+    public int[] insertNumberInArray(int[] a, int number) {
 
-        int rememberIndex = 0;
-        int[] clonedA = a.clone();
-        for (int i = 0; i < a.length - 1; i++) {
-            if (number > a[i] && number < a[i + 1]) {
-                a[i + 1] = number;
-                rememberIndex = i + 2;
-            }
+        int[] result = new int[a.length + 1];
+
+        for (int i = 0; i < a.length; i++) {
+            result[i] = a[i];
         }
-
-        for (int i = rememberIndex; i < a.length; i++) {
-            a[i] = clonedA[i - 1];
-        }
-
-        return a;
+        result[result.length - 1] = number;
+        Arrays.sort(result);
+        return result;
     }
 }
--- src/test/java/modul4/chapter4/TestSortierer.java
+++ src/test/java/modul4/chapter4/TestSortierer.java
@@ -10,14 +10,7 @@ public class TestSortierer {
     @Test
     void testInsertNumberInArray() {
         Sortierer sortierer = new Sortierer();
-        int[] actualArray = new int[8];
-        actualArray[0] = 17;
-        actualArray[1] = 32;
-        actualArray[2] = 45;
-        actualArray[3] = 53;
-        actualArray[4] = 61;
-        actualArray[5] = 62;
-        actualArray[6] = 75;
+        int[] actualArray = new int[]{17,32,45,53,61,62,75};
 
         assertArrayEquals(new int[]{17, 32, 45, 50, 53, 61, 62, 75}, sortierer.insertNumberInArray(actualArray, 50));
     }
