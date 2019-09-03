12c63fcac8008470fa8fe312c45f96350dcd706d
vlnguyen <van-loc.nguyen@immonet.de>
2019-09-03 11:25:20 +0200

    added unit test and delete method

--- src/main/java/modul4/chapter4/Sortierer.java
+++ src/main/java/modul4/chapter4/Sortierer.java
@@ -15,4 +15,21 @@ public class Sortierer {
         Arrays.sort(result);
         return result;
     }
+
+    public int[] deleteNumberFromArray(int[] a, int number) {
+        int[] clonedA = a.clone();
+        int rememberIndex = 0;
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] == number) {
+                rememberIndex = i;
+            }
+        }
+        for (int i = rememberIndex; i < a.length - 1; i++) {
+            a[i] = a[i + 1];
+        }
+
+        a[a.length - 1] = 0;
+        return a;
+    }
 }
--- src/test/java/modul4/chapter4/TestSortierer.java
+++ src/test/java/modul4/chapter4/TestSortierer.java
@@ -10,8 +10,17 @@ public class TestSortierer {
     @Test
     void testInsertNumberInArray() {
         Sortierer sortierer = new Sortierer();
-        int[] actualArray = new int[]{17,32,45,53,61,62,75};
+        int[] actualArray = new int[]{17, 32, 45, 53, 61, 62, 75};
 
         assertArrayEquals(new int[]{17, 32, 45, 50, 53, 61, 62, 75}, sortierer.insertNumberInArray(actualArray, 50));
+    }
+
+    @Test
+    void testDeleteNumberFromArray() {
+        Sortierer sortierer = new Sortierer();
+        int[] actualArray = new int[]{10, 7, 61, 4, 75, 34};
+
+        assertArrayEquals(new int[]{10, 7, 4, 75, 34, 0}, sortierer.deleteNumberFromArray(actualArray, 61));
+
     }
 }
