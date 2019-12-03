544e608a2db3afa2cbd5e74c672a706e78120e1f
vlnguyen <van-loc.nguyen@immonet.de>
2019-12-03 09:39:35 +0100

    prep main

--- src/main/java/modul6/ab04/Tabelle.java
+++ src/main/java/modul6/ab04/Tabelle.java
@@ -1,5 +1,6 @@
 package modul6.ab04;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Tabelle {
@@ -14,9 +15,17 @@ public class Tabelle {
     }
 
     public static void main(String[] args) {
+
+        Tabelle tabelle = new Tabelle();
+        List<Team> teams = new ArrayList<>();
+
         Team arsenal = new Team("Arsenal", 0, 1);
         Team barcelona = new Team("Barcelona", 0, 2);
         Team madrid = new Team("Madrid", 0, 3);
+
+        teams.add(arsenal);
+        teams.add(barcelona);
+        teams.add(madrid);
 
         Spielpaarung spielpaarung1 = new Spielpaarung(arsenal, barcelona,1,3);
         Spielpaarung spielpaarung2 = new Spielpaarung(barcelona, madrid,3,1);
